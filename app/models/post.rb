d5b509b18139da21f00286d3177f5b18272faab5
andrew950468 <andrewho950468@berkeley.edu>
2015-09-08 23:47:15 -0700

    small changes

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,10 +1,6 @@
 class Post < ActiveRecord::Base
 	has_attached_file :image, :styles => { :small => "150x150>" },
-                  :url  => "/assets/posts/:id/:style/:basename.:extension",
+                  :url  => "/image/:id",
                   :path => ":rails_root/app/assets/images"
-
-  validates_attachment :image, :presence => true,
-  :content_type => { :content_type => "image/jpg" },
-  :size => { :in => 0..100.kilobytes }
 end
 
