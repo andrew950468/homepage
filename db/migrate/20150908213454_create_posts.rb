2685a8e4a82449b8d9af08a8874b14e79b45a88e
patrick Yang <b98901136@ntu.edu.tw>
2015-09-16 22:03:15 +0800

    change the migration

--- db/migrate/20150908213454_create_posts.rb
+++ db/migrate/20150908213454_create_posts.rb
@@ -3,10 +3,9 @@ class CreatePosts < ActiveRecord::Migration
     create_table :posts do |t|
       t.string :title
       t.string :body
-      t.attachment :image
 
       t.timestamps null: false
-      add_attachment :posts, :image  
     end
+    add_attachment :posts, :image
   end
 end
