2282a247560ff6e68c1e9be896188ebfbdcc2f3d
andrew950468 <andrewho950468@berkeley.edu>
2015-09-20 16:19:53 -0700

    add timer..but seconds arenot displaying

---
+++ app/assets/javascripts/posts/timer.js
@@ -0,0 +1,33 @@
+function startTimer(duration, display) {
+    var start = Date.now(),
+        diff,
+        seconds;
+    function timer() {
+        // get the number of seconds that have elapsed since 
+        // startTimer() was called
+        diff = duration - (((Date.now() - start) / 1000) | 0);
+
+        // does the same job as parseInt truncates the float
+        seconds = (diff % 61) | 0;
+
+        //minutes = minutes < 10 ? "0" + minutes : minutes;
+        //seconds = seconds < 10 ? "0" + seconds : seconds;
+
+        display.textContent = seconds; 
+
+        if (diff <= 0) {
+            // add one second so that the count down starts at the full duration
+            // example 05:00 not 04:59
+            start = Date.now() + 1000;
+        }
+    };
+    // we don't want to wait a full second before the timer starts
+    timer();
+    setInterval(timer, 1000);
+}
+
+jQuery(function ($) {
+    var oneMinutes = 60,
+        display = $('#time');
+    startTimer(oneMinutes, display);
+});
--- db/schema.rb
+++ db/schema.rb
@@ -16,12 +16,12 @@ ActiveRecord::Schema.define(version: 20150918164424) do
   create_table "posts", force: :cascade do |t|
     t.string   "title"
     t.string   "body"
-    t.datetime "created_at",         null: false
-    t.datetime "updated_at",         null: false
     t.string   "image_file_name"
     t.string   "image_content_type"
     t.integer  "image_file_size"
     t.datetime "image_updated_at"
+    t.datetime "created_at",         null: false
+    t.datetime "updated_at",         null: false
   end
 
   create_table "users", force: :cascade do |t|
