d486f1a2d99b772577585aff8659402d53cb0043
patrick <neetp7k9@hotmail.com>
2015-10-25 17:57:27 +0900

    set aws-s3

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -78,13 +78,13 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
 
   #paperclip image database Amazon S3 config
-  config.paperclip_defaults = {
+config.paperclip_defaults = {
   :storage => :s3,
   :s3_credentials => {
-    :bucket => ENV['wtw-s3'],
-    :access_key_id => ENV['AKIAJO7RJS5C3KX42HWQ'],
-    :secret_access_key => ENV['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']
-    }
-
+    :bucket => ENV['S3_BUCKET_NAME'],
+    :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
+    :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
   }
+}
+  
 end
