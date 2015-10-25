b2b4c3e5380c66a7bdaeb8c5065cd32d9c0dec87
patrick <neetp7k9@hotmail.com>
2015-10-26 02:44:05 +0900

    database bug

--- db/schema.rb
+++ db/schema.rb
@@ -33,6 +33,8 @@ ActiveRecord::Schema.define(version: 20151025040124) do
     t.integer  "user_id"
   end
 
+  add_index "posts", ["user_id"], name: "index_posts_on_user_id"
+
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
     t.string   "encrypted_password",     default: "", null: false
