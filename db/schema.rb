146739439fe911e86cdf650eb3a2106c3e724a5f
LiouMichael <lioumichael@LiouMichaels-MacBook-Pro.local>
2015-10-24 21:12:14 -0700

    add user_id to posts

--- db/schema.rb
+++ db/schema.rb
@@ -11,18 +11,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150918164424) do
+ActiveRecord::Schema.define(version: 20151025040124) do
 
   create_table "posts", force: :cascade do |t|
     t.string   "title"
     t.string   "body"
+    t.datetime "created_at",         null: false
+    t.datetime "updated_at",         null: false
     t.string   "image_file_name"
     t.string   "image_content_type"
     t.integer  "image_file_size"
     t.datetime "image_updated_at"
-    t.datetime "created_at",         null: false
-    t.datetime "updated_at",         null: false
+    t.integer  "user_id"
   end
+
+  add_index "posts", ["user_id"], name: "index_posts_on_user_id"
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
