c5ec7014df147c627a429c7a1002c0fca20e3daf
patrick <neetp7k9@hotmail.com>
2015-10-26 00:47:15 +0900

    add_poster title

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -2,11 +2,12 @@ class MainController < ApplicationController
   def index
     @posts = Post.last(10)
     @post_meta=[]
-    plain_meta = {"hadComment"=> false,"buy"=> 0, "nbuy"=> 0}
-    meta = {"hadComment"=> true,"buy"=> 0, "nbuy"=> 0}
+    plain_meta = {"hadComment"=> false,"poster"=>"","buy"=> 0, "nbuy"=> 0}
+    meta = {"hadComment"=> true,"poster"=>"","buy"=> 0, "nbuy"=> 0}
     if user_signed_in?
        user = current_user
        @posts.each do |post|
+         meta["poster"] = User.find(post.user_id).email
          if Comment.find_by_user_id_and_post_id(user.id, post.id)
            nbuy = buy = 0
            Comment.where(post_id: post.id).find_each do |c|
@@ -22,6 +23,7 @@ class MainController < ApplicationController
        end
     else
        @posts.each do |post|
+         plain_meta["poster"] = User.find(post.user_id).email
          @post_meta.push plain_meta
        end
     end
